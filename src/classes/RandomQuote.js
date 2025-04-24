044bba85e4c9bc649008dd3fbb1d8398c9a705a6
Miron <mironkedich@gmail.com>
2025-04-24 18:55:46 +0300

    functionality with api

--- src/classes/RandomQuote.js
+++ src/classes/RandomQuote.js
@@ -1,11 +1,24 @@
-import quotes from '../data/quotes.js'
-import MathUtils from './MathUtils.js'
+// import quotes from '../data/quotes.js'
+// import MathUtils from '../utils/MathUtils.js'
 import Quote from './Quote.js'
 
 class RandomQuote {
-  static getRandomQuote() {
-    const {id, quote, author} = quotes[MathUtils.generateRandomInt(quotes.length)]
-    return new Quote(id, quote, author)
+  static async getRandomQuote() {
+  try {
+    const response = await fetch('https://quoteslate.vercel.app/api/quotes/random')
+    if (!response.ok) throw new Error(`Ошибка API: ${response.status}`)
+
+    const data = await response.json()
+    if (!data?.id || !data?.quote || !data?.author) {
+      throw new Error("Некорректные данные цитаты");
+    }
+
+    return new Quote(data.id, data.quote, data.author)
+  }
+  catch(error) {
+    console.error('Mistake in RandomQuote', error)
+    throw error
+  }
   }
 }
 
--- src/classes/RandomQuotesApp.js
+++ src/classes/RandomQuotesApp.js
@@ -10,13 +10,18 @@ class RandomQuotesApp {
     this.init()
   }
 
-  getRandomQuote() {
-    const randomQuote = RandomQuote.getRandomQuote()
-    this.currentQuote = randomQuote
-
-    const {text, author} = this.currentQuote
-    this.quoteTextCont.textContent = text
-    this.quoteAuthorCont.textContent = author 
+  async getRandomQuote() {
+    try {
+      const randomQuote = await RandomQuote.getRandomQuote()
+      this.currentQuote = randomQuote
+      console.log(this.currentQuote)
+      const {text, author} = this.currentQuote
+      this.quoteTextCont.textContent = text
+      this.quoteAuthorCont.textContent = author
+    }
+    catch(error) {
+      console.error('Mistake in getRandomQuote', error)
+    }
   }
 
   init() {
